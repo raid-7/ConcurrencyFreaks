68842588146ed0c341dc410de00ae33820038dde
Raed Romanov <raid_r@mail.ru>
2022-05-22 19:36:51 +0300

    Change benchmarks script

--- CPP/lprq/benchmarks.sh
+++ CPP/lprq/benchmarks.sh
@@ -9,6 +9,8 @@ ED_BENCH="$1/bench-enq-deq"
   "$PC_BENCH" -f cpp-res-p1c2.csv -t 1 2 2 4 4 8 6 12 8 16 10 20 14 28 -w 8
   "$PC_BENCH" -f cpp-res-p2c1b.csv -t 2 1 4 2 8 4 12 6 16 8 20 10 28 14 -w 8 -b
   "$PC_BENCH" -f cpp-res-p1c2b.csv -t 1 2 2 4 4 8 6 12 8 16 10 20 14 28 -w 8 -b
-  "$PC_BENCH" -f cpp-res-mpsc.csv -t 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 1 -w 24
-  "$PC_BENCH" -f cpp-res-spmc.csv -t 1 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 -w 24
+  "$PC_BENCH" -f cpp-res-mpsc.csv -t 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 1 -w 8 24
+  "$PC_BENCH" -f cpp-res-spmc.csv -t 1 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 -w 8 24
+  "$PC_BENCH" -f cpp-res-mpscb.csv -t 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 1 -w 8 24 -b
+  "$PC_BENCH" -f cpp-res-spmcb.csv -t 1 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 -w 8 24 -b
 } > cpp-res.txt 2>&1
