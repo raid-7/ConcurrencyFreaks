f7e0c3dbd5bda537eee78c20ad5c36470a1fc5c0
Raed Romanov <raid_r@mail.ru>
2022-05-23 00:38:30 +0300

    Add run of symmetric benchmark to the script

--- CPP/lprq/benchmarks.sh
+++ CPP/lprq/benchmarks.sh
@@ -13,4 +13,5 @@ ED_BENCH="$1/bench-enq-deq"
   "$PC_BENCH" -f cpp-res-spmc.csv -t 1 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 1 52 1 62 -w 8 24
   "$PC_BENCH" -f cpp-res-mpscb.csv -t 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 1 52 1 62 1 -w 8 24 -b
   "$PC_BENCH" -f cpp-res-spmcb.csv -t 1 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 1 52 1 62 -w 8 24 -b
+  "$ED_BENCH" -f cpp-res-enq-deq.csv -t 1 2 4 8 16 24 32 40 48 64 -w 8
 } > cpp-res.txt 2>&1
