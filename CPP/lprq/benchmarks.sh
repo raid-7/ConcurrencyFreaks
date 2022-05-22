0a08d94738a1e3945bb74cc03f0d993b08850ccb
Raed Romanov <raid_r@mail.ru>
2022-05-22 18:56:56 +0300

    Improve benchmarks script

--- CPP/lprq/benchmarks.sh
+++ CPP/lprq/benchmarks.sh
@@ -3,8 +3,12 @@
 PC_BENCH="$1/bench-prod-cons"
 ED_BENCH="$1/bench-enq-deq"
 
-"$PC_BENCH" -f cpp-res-p1c1.csv -t 1 1 2 2 4 4 8 8 12 12 16 16 20 20 -w 8 > cpp-res-p1c1.txt 2>&1
-"$PC_BENCH" -f cpp-res-p2c1.csv -t 2 1 4 2 8 4 12 6 16 8 20 10 28 14 -w 8 > cpp-res-p2c1.txt 2>&1
-"$PC_BENCH" -f cpp-res-p1c2.csv -t 1 2 2 4 4 8 6 12 8 16 10 20 14 28 -w 8 > cpp-res-p1c2.txt 2>&1
-"$PC_BENCH" -f cpp-res-mpsc.csv -t 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 1 -w 8 > cpp-res-mpsc.txt 2>&1
-"$PC_BENCH" -f cpp-res-spmc.csv -t 1 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 -w 8 > cpp-res-spmc.txt 2>&1
+{
+  "$PC_BENCH" -f cpp-res-p1c1.csv -t 1 1 2 2 4 4 8 8 12 12 16 16 20 20 -w 8
+  "$PC_BENCH" -f cpp-res-p2c1.csv -t 2 1 4 2 8 4 12 6 16 8 20 10 28 14 -w 8
+  "$PC_BENCH" -f cpp-res-p1c2.csv -t 1 2 2 4 4 8 6 12 8 16 10 20 14 28 -w 8
+  "$PC_BENCH" -f cpp-res-p2c1b.csv -t 2 1 4 2 8 4 12 6 16 8 20 10 28 14 -w 8 -b
+  "$PC_BENCH" -f cpp-res-p1c2b.csv -t 1 2 2 4 4 8 6 12 8 16 10 20 14 28 -w 8 -b
+  "$PC_BENCH" -f cpp-res-mpsc.csv -t 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 1 -w 24
+  "$PC_BENCH" -f cpp-res-spmc.csv -t 1 1 1 2 1 4 1 8 1 16 1 24 1 32 1 42 -w 24
+} > cpp-res.txt 2>&1
