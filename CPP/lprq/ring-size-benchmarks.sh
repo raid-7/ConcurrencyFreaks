ca68071c6b1df86e0f828d56262dadca99eb221a
Raed Romanov <raid_r@mail.ru>
2022-05-24 02:06:37 +0300

    Fix ring size sensitivity benchmark script

--- CPP/lprq/ring-size-benchmarks.sh
+++ CPP/lprq/ring-size-benchmarks.sh
@@ -7,7 +7,7 @@ ED_BENCH="$1/bench-enq-deq"
   "$PC_BENCH" -f cpp-res-rs-p1c1.csv -t 8 8 24 24 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384
   "$PC_BENCH" -f cpp-res-rs-p2c1.csv -t 10 5 32 16 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384
   "$PC_BENCH" -f cpp-res-rs-p1c2.csv -t 5 10 16 32 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384
-  "$PC_BENCH" -f cpp-res-rs-p2c1.csv -t 10 5 32 16 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -b
-  "$PC_BENCH" -f cpp-res-rs-p1c2.csv -t 5 10 16 32 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -b
+  "$PC_BENCH" -f cpp-res-rs-p2c1b.csv -t 10 5 32 16 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -b
+  "$PC_BENCH" -f cpp-res-rs-p1c2b.csv -t 5 10 16 32 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -b
   "$ED_BENCH" -f cpp-res-rs-enq-deq.csv -t 16 48 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384
 } > cpp-res-rs.txt 2>&1
