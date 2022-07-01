65a5156b5a9329c0877d97283e96688282b91d86
Raed Romanov <raid_r@mail.ru>
2022-07-01 19:56:01 +0300

    Improve ring size benchmarks script

--- CPP/lprq/ring-size-benchmarks.sh
+++ CPP/lprq/ring-size-benchmarks.sh
@@ -3,17 +3,14 @@
 PC_BENCH="$1/bench-prod-cons"
 ED_BENCH="$1/bench-enq-deq"
 
-if [[ -z "$2" ]]; then
-  QUEUE_FILTER='.*'
-else
-    QUEUE_FILTER="$2"
-fi
+shift
 
 {
-  "$PC_BENCH" -f cpp-res-rs-p1c1.csv -t 8 8 24 24 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -q "$QUEUE_FILTER"
-  "$PC_BENCH" -f cpp-res-rs-p2c1.csv -t 10 5 32 16 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -q "$QUEUE_FILTER"
-  "$PC_BENCH" -f cpp-res-rs-p1c2.csv -t 5 10 16 32 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -q "$QUEUE_FILTER"
-  "$PC_BENCH" -f cpp-res-rs-p2c1b.csv -t 10 5 32 16 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -b -q "$QUEUE_FILTER"
-  "$PC_BENCH" -f cpp-res-rs-p1c2b.csv -t 5 10 16 32 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -b -q "$QUEUE_FILTER"
-  "$ED_BENCH" -f cpp-res-rs-enq-deq.csv -t 16 48 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -q "$QUEUE_FILTER"
+  "$PC_BENCH" -f cpp-res-rs-p1c1.csv -t 8 8 24 24 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 "$@"
+  "$PC_BENCH" -f cpp-res-rs-p1c1b.csv -t 8 8 24 24 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -b "$@"
+  "$PC_BENCH" -f cpp-res-rs-p2c1.csv -t 10 5 32 16 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 "$@"
+  "$PC_BENCH" -f cpp-res-rs-p1c2.csv -t 5 10 16 32 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 "$@"
+  "$PC_BENCH" -f cpp-res-rs-p2c1b.csv -t 10 5 32 16 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -b "$@"
+  "$PC_BENCH" -f cpp-res-rs-p1c2b.csv -t 5 10 16 32 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 -b "$@"
+  "$ED_BENCH" -f cpp-res-rs-enq-deq.csv -t 16 48 -w 8 -r 16 64 256 512 1024 2048 4096 8192 16384 "$@"
 } > cpp-res-rs.txt 2>&1
